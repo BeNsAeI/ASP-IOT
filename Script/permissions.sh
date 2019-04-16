89da14e8ebb3c8062c86136de561f8c09837614c
mowerym <mowerym@oregonstate.edu>
2019-04-15 19:26:14 -0700

    Adding scripts
    These are the two scripts that are talked about in the instalation guide.

---
+++ Script/permissions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+chmod 751 *.php
+chmod 771 database.php
+chmod 775 css
+chmod 775 css/*
+chmod 775 images
+chmod 775 images/*
+chmod 775 js
+chmod 775 js/*
+chmod 771 errors
+chmod 771 errors/*
